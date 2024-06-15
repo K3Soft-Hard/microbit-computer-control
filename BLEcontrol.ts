efc7994142fbd78b21de06e609c9a8acf6cd3f70
K3Soft-Hard <159473672+K3Soft-Hard@users.noreply.github.com>
2024-06-15 12:47:51 +0200

    Update BLEcontrol.ts

--- BLEcontrol.ts
+++ BLEcontrol.ts
@@ -6,40 +6,42 @@ namespace Control {
         cyan=1
     }
 
-//%blockId="MCCsendToChat"
-//%block="send $message to chat || $color"
-//%group="Minecraft"
-//%advanced=true
-export function sendToChat(message: string, color: Colors) {
-    switch (color) {
-        case Colors.ledOn:
-            keyboard.sendString("t")
-            basic.pause(80)
-            keyboard.sendString("message")
-            basic.pause(25)
-            keyboard.sendString(keyboard.keys(keyboard._Key.enter))
-            keyboard.sendString(keyboard.keys(keyboard._Key.escape))
-            break
-        case ScreenMode.ledOff:
-            basic.clearScreen()
-            break
-        case ScreenMode.chessboard:
-            basic.showLeds(`
-                    # . # . #
-                    . # . # .
-                    # . # . #
-                    . # . # .
-                    # . # . #
-                `)
-            break
-        case ScreenMode.invertedChessboard:
-            basic.showLeds(`
-                    . # . # .
-                    # . # . #
-                    . # . # .
-                    # . # . #
-                    . # . # .
-                `)
-            break
-    
+    //%blockId="MCCsendToChat"
+    //%block="send $message to chat || $color"
+    //%group="Minecraft"
+    //%advanced=true
+    export function sendToChat(message: string, color: Colors) {
+        switch (color) {
+            case Colors.none:
+                keyboard.sendString("t")
+                basic.pause(80)
+                keyboard.sendString(message)
+                basic.pause(25)
+                keyboard.sendString(keyboard.keys(keyboard._Key.enter))
+                keyboard.sendString(keyboard.keys(keyboard._Key.escape))
+                break
+            case Colors.cyan:
+                keyboard.sendString("t")
+                basic.pause(80)
+                keyboard.sendString("§b" + message)
+                basic.pause(25)
+                keyboard.sendString(keyboard.keys(keyboard._Key.enter))
+                keyboard.sendString(keyboard.keys(keyboard._Key.escape))
+                break
+        }   
+    }
+    //%blockId="MCCopenChat"
+    //%block="open chat"
+    //%group="Minecraft"
+    //%advanced=true
+    export function openChat () {
+        keyboard.sendString("t")
+    }    
+    //%blockId="MCCinventory"
+    //%block="inventory"
+    //%group="Minecraft"
+    //%advanced=true
+    export function inventory() {
+        keyboard.sendString("e")
+    }
 }
